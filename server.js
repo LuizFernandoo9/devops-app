56d7ca6bef63cbfe72c8df1ba1d79f7b27ea07d4
luiz.de.sena.barbosa <luiz.de.sena.barbosa@avanade.com>
2025-12-28 21:45:22 -0300

    fix: ajuste de porta

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 
 const server = http.createServer((req, res) => {
   res.end("Hello World - Projeto DevOps");
