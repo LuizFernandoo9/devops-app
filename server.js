20af693c272cd97fb1a93d0217d8458cae9b8368
luiz.de.sena.barbosa <luiz.de.sena.barbosa@avanade.com>
2025-12-29 20:44:54 -0300

    testando deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World - Projeto DevOps - teste de deploy");
+  res.end("Hello World - Projeto DevOps - deploy");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
