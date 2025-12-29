f17145772f2229be8b3435874da8ae44c155a68e
luiz.de.sena.barbosa <luiz.de.sena.barbosa@avanade.com>
2025-12-28 22:25:30 -0300

    ajustes para o container

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require("http");
+const http = require("http"); 
 const PORT = 80;
 
 const server = http.createServer((req, res) => {
   res.end("Hello World - Projeto DevOps");
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Rodando na porta ${PORT}`);
 });
