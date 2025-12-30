4cd35f61598a63aae03c4f7550882005d64057bc
luiz.de.sena.barbosa <luiz.de.sena.barbosa@avanade.com>
2025-12-29 21:21:55 -0300

    commit apresentação

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World - Projeto DevOps - apresentação");
+  res.end("Hello World - Projeto DevOps - apresentação final");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
