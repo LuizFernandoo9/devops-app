fa111ccf0d3a37c1c968a85bd25267763cf3b778
luiz.de.sena.barbosa <luiz.de.sena.barbosa@avanade.com>
2025-12-29 21:14:29 -0300

    apresentação

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World - Projeto DevOps - alteração");
+  res.end("Hello World - Projeto DevOps - apresentação");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
